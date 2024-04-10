b5104c5fa5e3462a3148b2ca7d06a67012d3c509
Owen <c20392651@mytudublin.ie>
2024-04-10 12:16:54 +0100

    Feat: Header & Profile Component

--- src/components/common/header.js
+++ src/components/common/header.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { globalStyles } from "../../assets/style/globalStyle";
 import { StyleSheet, Text, View } from "react-native-web";
+import ProfileIcon from '../../assets/svg/profileIcon';
+
 
 export default function Header() {
   return (
-    <View style={globalStyles.header}>
-      <p>Header</p>
+    <View style={globalStyles.headerContainer}>
+      <ProfileIcon style={globalStyles.headerImage} />
+      <Text style={globalStyles.headerText}>Good Morning, <Text style={{fontStyle: 'italic'}}>Abdul</Text></Text>
     </View>
   );
 }
