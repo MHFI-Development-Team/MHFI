05f7e49ce87a00bad11eafd6d7e1d5d2cca799a9
Jeremy Odor <102603930+Jeremy2408@users.noreply.github.com>
2024-04-25 14:11:32 +0100

    Add files via upload

---
+++ SettingsScreen.js
@@ -0,0 +1,51 @@
+import { View, Text, TouchableOpacity } from 'react-native'
+import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { MaterialIcons} from "@expo/vector-icons";
+
+const SettingsScreen = ({navigation}) => {
+  return (
+    <SafeAreaView style={{
+      flex: 1
+      
+    }}>
+      <View style={{
+        marginHorizontal: 12,
+        flexDirection: "row" ,
+        justifyContent: "center"
+      }}>
+        <TouchableOpacity
+         onPress={()=>navigation.goBack()}
+         style={{
+          position: "absolute",
+          left: 0
+
+         }}
+        >
+
+          <MaterialIcons
+          name="keyboard-arrow-left"
+          size={24}
+          />
+          <Text style={{...FONTS.h3}}>Settings</Text>
+
+
+
+        </TouchableOpacity>
+        
+
+
+      </View>
+
+      
+
+
+
+
+
+</SafeAreaView>
+
+  )
+}
+
+export default SettingsScreen
