a6cf1c6ed2b59970202bf7e97c7092dee7d62e9c
DemiAina <100835583+DemiAina@users.noreply.github.com>
2024-04-26 13:27:03 +0100

    Feat: Added themes for use to have a consistent style

---
+++ assets/theme.js
@@ -0,0 +1,20 @@
+export const colours = {
+  mainRed: "#DC3535",
+  secondaryOrange: "#D17842",
+  darkBlue: "#0C0F14",
+  darkGray: "#252A32",
+  mediumGray: "#52555A",
+  lightGray: "#AEAEAE",
+  white: "#FFFFFF",
+};
+
+export const textStyles = {
+  "3xl": 28,
+  "2xl": 24,
+  "xl": 20,
+  "l": 18,
+  "m": 16,
+  "s: 14,
+  xs: 12,
+  xxs: 10,
+};
