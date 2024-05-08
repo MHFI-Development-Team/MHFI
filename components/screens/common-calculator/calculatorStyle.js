e8f9fb18871a3265ae9bd430caa44c911931cd29
AdamGrennan <adamgr5000@gmail.com>
2024-05-08 12:46:19 +0100

    Update calculatorStyle.js

--- components/screens/common-calculator/calculatorStyle.js
+++ components/screens/common-calculator/calculatorStyle.js
@@ -1,4 +1,6 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet,  Dimensions, } from "react-native";
+
+const screenWidth = Dimensions.get("window").width;
 
 const styles = StyleSheet.create({
   container: {
