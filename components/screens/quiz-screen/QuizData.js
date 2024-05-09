ee5c2654ee93a91f16adda42760a59fc840c0c5e
AdamGrennan <adamgr5000@gmail.com>
2024-05-09 13:23:48 +0100

    Update to quiz added 5 health questions

--- components/screens/quiz-screen/QuizData.js
+++ components/screens/quiz-screen/QuizData.js
@@ -2,23 +2,34 @@ const QuizData = () => {
   const questions = [
     {
       id: 1,
-      question: "What is the capital of France?",
-      options: ["Paris", "Berlin", "London", "Madrid"],
-      correctAnswer: "Paris"
+      question: "What percentage of men worldwide smoke cigarettes?",
+      options: ["25%", "35%", "45%", "55%"],
+      correctAnswer: "35%"
     },
     {
       id: 2,
-      question: "What is the capital of Ireland?",
-      options: ["Cork", "Dublin", "Galway", "Meath"],
-      correctAnswer: "Dublin"
+      question: "What percentage of men are overweight or obese globally?",
+      options: ["30%", "40%", "50%", "60%"],
+      correctAnswer: "40%"
     },
     {
       id: 3,
-      question: "Who is the best football team?",
-      options: ["Liverpool", "Chelsea", "Man Utd", "Man City"],
-      correctAnswer: "Goalkeeper"
+      question: "What is the recommended daily intake of fiber for adult men to promote digestive health?",
+      options: ["10 grams", "20 grams", "30 grams", "40 grams"],
+      correctAnswer: "30 grams"
     },
-    // Add more questions as needed
+    {
+      id: 4,
+      question: "What are some common symptoms of male depression?",
+      options: ["Feelings of sadness or hopelessness", "Loss of interest in hobbies", "Fatigue or decreased energy", "All of the above"],
+      correctAnswer: "All of the above"
+    },
+    {
+      id: 5,
+      question: "What is the recommended amount of physical activity for adult men per week according to health guidelines?",
+      options: ["30 minutes", "60 minutes", "90 minutes", "120 minutes"],
+      correctAnswer: "120 minutes"
+    },
   ];
 
   return questions;
